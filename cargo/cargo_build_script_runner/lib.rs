6804537e4c97ccf27a2400ab9506cbf85e4e6932
Jalal El Mansouri <jelmansouri@google.com>
2020-06-25 12:16:28 -0400

    Use a c++ wrapper to call rustc instead of shell

--- cargo/cargo_build_script_runner/lib.rs
+++ cargo/cargo_build_script_runner/lib.rs
@@ -121,7 +121,7 @@ impl BuildScriptOutput {
                 }
             })
             .collect::<Vec<_>>()
-            .join(" ")
+            .join("\n")
     }
 
     /// Convert a vector of [BuildScriptOutput] into a list of dependencies environment variables.
@@ -143,7 +143,7 @@ impl BuildScriptOutput {
                 }
             })
             .collect::<Vec<_>>()
-            .join(" ")
+            .join("\n")
     }
 
     /// Convert a vector of [BuildScriptOutput] into a flagfile.
@@ -161,8 +161,8 @@ impl BuildScriptOutput {
             }
         }
         CompileAndLinkFlags {
-            compile_flags: compile_flags.join(" "),
-            link_flags: link_flags.join(" ").replace(exec_root, "${EXEC_ROOT}"),
+            compile_flags: compile_flags.join("\n"),
+            link_flags: link_flags.join("\n").replace(exec_root, "<EXEC_ROOT>"),
         }
     }
 }
@@ -204,19 +204,19 @@ cargo:version_number=1010107f
 
         assert_eq!(
             BuildScriptOutput::to_dep_env(&result, "my-crate-sys"),
-            "DEP_MY_CRATE_VERSION=123 DEP_MY_CRATE_VERSION_NUMBER=1010107f".to_owned()
+            "DEP_MY_CRATE_VERSION=123\nDEP_MY_CRATE_VERSION_NUMBER=1010107f".to_owned()
         );
         assert_eq!(
             BuildScriptOutput::to_env(&result),
-            "FOO=BAR BAR=FOO".to_owned()
+            "FOO=BAR\nBAR=FOO".to_owned()
         );
         assert_eq!(
             BuildScriptOutput::to_flags(&result, "/some/absolute/path"),
             CompileAndLinkFlags {
                 // -Lblah was output as a rustc-flags, so even though it probably _should_ be a link
                 // flag, we don't treat it like one.
-                compile_flags: "-Lblah --cfg=feature=awesome".to_owned(),
-                link_flags: "-lsdfsdf -L${EXEC_ROOT}/bleh".to_owned(),
+                compile_flags: "-Lblah\n--cfg=feature=awesome".to_owned(),
+                link_flags: "-lsdfsdf\n-L<EXEC_ROOT>/bleh".to_owned(),
             }
         );
     }
---
+++ rust/private/rust_tool_wrapper/system.h
@@ -0,0 +1,47 @@
+#ifndef RUST_PRIVATE_RUSTC_WRAPPER_SYSTEM_H_
+#define RUST_PRIVATE_RUSTC_WRAPPER_SYSTEM_H_
+
+#include <string>
+#include <vector>
+
+#if defined(_WIN32) && defined(UNICODE)
+#define RTW_WIN_UNICODE
+#endif  // defined(_WIN32) && defined(UNICODE)
+
+#if defined(RTW_WIN_UNICODE)
+#define RTW_SYS_STR_LITERAL(str) L##str
+#else
+#define RTW_SYS_STR_LITERAL(str) str
+#endif
+
+namespace rust_tool_wrapper {
+
+class System {
+ public:
+#if defined(RTW_WIN_UNICODE)
+  using StrType = std::wstring;
+#else
+  using StrType = std::string;
+#endif  // defined(RTW_WIN_UNICODE)
+
+  using Arguments = std::vector<StrType>;
+  using EnvironmentBlock = std::vector<StrType>;
+
+ public:
+  static StrType ToStrType(const std::string& string);
+
+  static StrType ComposeEnvironmentVariable(const StrType& key,
+                                            const StrType& value);
+
+  static StrType GetWorkingDirectory();
+  static StrType JoinPaths(const StrType& path1, const StrType& path2);
+
+  static int Exec(const StrType& executable, const Arguments& arguments,
+                  const EnvironmentBlock& environment_block);
+
+  static int UnTar(const StrType& tar_file, const StrType& out_dir);
+};
+
+}  // namespace rust_tool_wrapper
+
+#endif  // RUST_PRIVATE_RUSTC_WRAPPER_SYSTEM_H_
